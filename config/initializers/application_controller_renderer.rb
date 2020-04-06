0a2fecd346fe503daa83eb90613b6a37c44a8f48
hojokaban <hojokaban@gmail.com>
2020-04-06 15:56:49 +0900

    [CommentAdded]meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e6eac52988d305687eb9c0c37c8a1942e57317098148286f0a4864781d118a184f983e4c1223cb065d7e8cec5ac460557476c4ec7f659ccc44e59ff99cfa5e26'
